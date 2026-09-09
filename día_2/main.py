35d97fdcf3b7d84646d1ca435897eaafa45f7005
Gonzalo-6 <g-arranz@hotmail.com>
2026-09-09 11:07:16 +0200

    guardando

---
+++ día_2/main.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import os
+
+
+from main import open_connection
+
+json_path = "config/config.json"
+print("Existe archivo:", os.path.exists(json_path))
+json_path = "config/config.json"
+print("Existe archivo:", os.path.exists(json_path))
+
+def get_order_total(json_path, order_id):
+    conn = open_connection(json_path)
+
+    if not conn:
+        return None
+
+    query = f"""
+        SELECT 
+            order_id,
+            SUM(unit_price * quantity * (1 - discount)) AS total_price
+        FROM order_details
+        WHERE order_id = {order_id}
+        GROUP BY order_id;
+    """
+
+    try:
+        df = pd.read_sql(query, conn)
+        return df
+
+    except Exception as e:
+        print("Error en la query:", e)
+        return None
+
+    finally:
+        conn.close()
+
+
+df = get_order_total("config/config.json", 10248)
+
+print(df)
